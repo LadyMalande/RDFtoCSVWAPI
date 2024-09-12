23c3f3150a4acbb0ea8cc7e62dad4d3e8219a983
Tereza Miklóšová <t.miklosova@seznam.cz>
2024-09-12 17:28:21 +0200

    CORS handling specific origins

--- src/main/java/org/rdftocsvconverter/RDFtoCSVW/config/WebConfig.java
+++ src/main/java/org/rdftocsvconverter/RDFtoCSVW/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:4000", "https://ladymalande.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Content-Disposition")
