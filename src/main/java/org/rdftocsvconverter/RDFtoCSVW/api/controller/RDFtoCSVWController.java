00a693a371e5fa1ff4d3e36e0ff4a356424bb9aa
Tereza Miklóšová <t.miklosova@seznam.cz>
2024-12-19 07:32:49 +0100

    Controller with @Parameter with Enum

--- src/main/java/org/rdftocsvconverter/RDFtoCSVW/api/controller/RDFtoCSVWController.java
+++ src/main/java/org/rdftocsvconverter/RDFtoCSVW/api/controller/RDFtoCSVWController.java
@@ -81,7 +81,7 @@ public class RDFtoCSVWController {
     @PostMapping("/rdftocsvw")
     public byte[] getCSVW(@RequestParam("file") MultipartFile file, @RequestParam("fileURL") String fileURL,
                           //@Parameter(description = "The number of CSV tables created during conversion", schema = @Schema(implementation = TableChoice.class))
-                          @Parameter(description = "The number of CSV tables created during conversion", example = "ONE")
+                          //@Parameter(description = "The number of CSV tables created during conversion", example = "ONE")
                               @RequestParam TableChoice choice){
         System.out.println("Got params for /rdftocsvw : " + file + " fileURL = " + fileURL + " choice=" + choice);
         briefingController.sendManualBriefing("At the beginning of the /rdftocsvw method");
